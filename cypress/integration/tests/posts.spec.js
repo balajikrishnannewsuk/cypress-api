3d633325ec98d88f0f138bfaffff8263b5086998
Krishnan <balajikrishnan0815@gmail.com>
2022-02-02 16:21:06 +0530

    Adding stunnel in CircleCI

--- cypress/integration/tests/posts.spec.js
+++ cypress/integration/tests/posts.spec.js
@@ -3,18 +3,18 @@
 Cypress.config("baseUrl", "http://jsonplaceholder.typicode.com");
 
 describe('Post API', () => {
-  it('loads all the posts successfully', () => {
+  it('loads all the posts successfully - This is a GET request', () => {
     cy.api({ url: '/posts' }).then((res) => {
       expect(res.status).to.equal(200);
       expect(res.body.length).to.equal(100);
     });
   });
 
-  it('loads the correct amount of posts per user', () => {
+  it('loads the correct amount of posts per user - This is a GET request ', () => {
     cy.api({ url: '/posts?userId=1' }).its('body').should('have.length', 10);
   });
 
-  it('should add a new post successfully', () => {
+  it('should add a new post successfully - This is a POST request', () => {
     cy.api({
       method: 'POST',
       url: '/posts?userId=1',
@@ -29,7 +29,7 @@ describe('Post API', () => {
     });
   });
 
-  it('should delete a post successfully', () => {
+  it('should delete a post successfully - This is a DELETE request', () => {
     cy.api({ method: 'DELETE', url: '/posts/1' })
       .its('status')
       .should('be.equal', 200);
